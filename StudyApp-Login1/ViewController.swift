bf07e6f4ce88d4265e44648ef8183adb493f4608
tahara yo <tahara.you@ring-tech.jp>
2023-10-27 16:08:10 +0900

    Initial Commit

---
+++ StudyApp-Login1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyApp-Login1
+//
+//  Created by yotahara on 2023/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
